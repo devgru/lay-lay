68c11e508f7f367268b28eeec08232fa256d6a31
Dima Devgru <git@devg.ru>
2025-02-23 21:02:41 +0200

    Slightly more complex example of elements stacking

--- packages/demos/src/App/pages/FitMe.tsx
+++ packages/demos/src/App/pages/FitMe.tsx
@@ -5,23 +5,43 @@ import {
 } from 'react-svg-guides';
 
 export const FitMe = () => {
-  const gRef = useRefWithSize<SVGCircleElement>();
+  const rootRef = useRefWithSize<SVGCircleElement>();
+  const shapesRef = useRefWithSize<SVGCircleElement>();
   const htmlRef = useRefWithSize<HTMLDivElement>();
 
   return (
-    <svg width={gRef.width} height={gRef.height}>
-      <g ref={gRef}>
+    <svg width={rootRef.width} height={rootRef.height}>
+      <g ref={rootRef}>
         <StackLayout stackDirection="vertical">
-          <HTML ref={htmlRef} width={300} height={htmlRef.height} style={{textAlign: 'center'}}>
+          <HTML
+            ref={htmlRef}
+            width={shapesRef.width}
+            height={htmlRef.height}
+            style={{ textAlign: 'center' }}
+          >
             <p>
-              This paragraph and these circles are positioned using <code>StackLayout</code>, based on their size.
+              This paragraph and these elements are positioned using{' '}
+              <code>StackLayout</code>, based on their size.
             </p>
           </HTML>
-          <StackLayout stackDirection="horizontal">
-            <circle r={50} fill="red" />
-            <circle r={50} fill="green" />
-            <circle r={50} fill="blue" />
-          </StackLayout>
+          <g ref={shapesRef}>
+            <StackLayout stackDirection="horizontal">
+              <circle r={50} fill="red" />
+              <rect
+                width={71}
+                height={71}
+                fill="green"
+                transform="translate(100 -1000) rotate(45) translate(100)"
+              />
+              <circle r={50} fill="blue" />
+              <rect
+                width={71}
+                height={71}
+                fill="yellow"
+                transform="scale(1.41)"
+              />
+            </StackLayout>
+          </g>
         </StackLayout>
       </g>
     </svg>
