a91bc8ff6bd27b48a9039cba4d262c7511b632cc
Dima Devgru <git@devg.ru>
2025-07-26 12:10:27 +0300

    chore(core): remove optional hook deps

--- packages/core/src/hooks/useSizeObserver.ts
+++ packages/core/src/hooks/useSizeObserver.ts
@@ -5,13 +5,10 @@ export const useSizeObserver = (): SizeObserver => {
   const [width, setWidth] = useState<number | undefined>(undefined);
   const [height, setHeight] = useState<number | undefined>(undefined);
 
-  const setSize = useCallback(
-    (width: number, height: number) => {
-      setWidth(width);
-      setHeight(height);
-    },
-    [setWidth, setHeight],
-  );
+  const setSize = useCallback((width: number, height: number) => {
+    setWidth(width);
+    setHeight(height);
+  }, []);
 
   return {
     width,
